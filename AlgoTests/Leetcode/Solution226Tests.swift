c93f1501e8e199a1eb1dcb0aac59bb891cf6882d
BBC6BAE9 <bbc6bae9@gmail.com>
2025-04-17 13:56:41 +0800

    feat(binary tree): 前中后序遍历解决二叉树翻转问题

--- AlgoTests/Leetcode/Solution226Tests.swift
+++ AlgoTests/Leetcode/Solution226Tests.swift
@@ -9,7 +9,7 @@ import Testing
 
 struct Solution226Tests {
     
-    @Test func testInvertTree() async throws {
+    @Test func testInvertTree1() async throws {
         // [4,2,7,1,3,6,9]
 
         let rootNode = TreeNode(4)
@@ -20,7 +20,53 @@ struct Solution226Tests {
         rootNode.right?.left = TreeNode(6)
         rootNode.right?.right = TreeNode(9)
         
-        let invertTreeNode = Solution226().invertTree(rootNode)
+        let invertTreeNode = Solution226().invertTree1(rootNode)
+        
+        let ret1 = invertTreeNode?.left?.left?.val
+        let want1 = 9
+        
+        let ret2 = invertTreeNode?.right?.right?.val
+        let want2 = 1
+        
+        assert(ret1 == want1)
+        assert(ret2 == want2)
+    }
+    
+    @Test func testInvertTree2() async throws {
+        // [4,2,7,1,3,6,9]
+
+        let rootNode = TreeNode(4)
+        rootNode.left = TreeNode(2)
+        rootNode.right = TreeNode(7)
+        rootNode.left?.left = TreeNode(1)
+        rootNode.left?.right = TreeNode(3)
+        rootNode.right?.left = TreeNode(6)
+        rootNode.right?.right = TreeNode(9)
+        
+        let invertTreeNode = Solution226().invertTree2(rootNode)
+        
+        let ret1 = invertTreeNode?.left?.left?.val
+        let want1 = 9
+        
+        let ret2 = invertTreeNode?.right?.right?.val
+        let want2 = 1
+        
+        assert(ret1 == want1)
+        assert(ret2 == want2)
+    }
+    
+    @Test func testInvertTree3() async throws {
+        // [4,2,7,1,3,6,9]
+
+        let rootNode = TreeNode(4)
+        rootNode.left = TreeNode(2)
+        rootNode.right = TreeNode(7)
+        rootNode.left?.left = TreeNode(1)
+        rootNode.left?.right = TreeNode(3)
+        rootNode.right?.left = TreeNode(6)
+        rootNode.right?.right = TreeNode(9)
+        
+        let invertTreeNode = Solution226().invertTree3(rootNode)
         
         let ret1 = invertTreeNode?.left?.left?.val
         let want1 = 9
