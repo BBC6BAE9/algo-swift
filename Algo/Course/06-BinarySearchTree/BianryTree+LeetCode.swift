99b6830f89367497122f2aac5434ce58f4e24a05
huanghong <bbc6bae9@gmail.com>
2025-04-19 12:59:46 +0800

    feature(bst): homework

---
+++ Algo/Course/06-BinarySearchTree/BianryTree+LeetCode.swift
@@ -0,0 +1,23 @@
+//
+//  BianryTree+LeetCode.swift
+//  Algo
+//
+//  Created by hong on 4/19/25.
+//
+
+import Foundation
+
+extension BianryTree {
+    // 226.翻转二叉树：https://leetcode.cn/problems/invert-binary-tree/description/
+    // 450.删除二叉搜索树中的节点：https://leetcode.cn/problems/delete-node-in-a-bst/description/
+    // 701.二叉搜索树中的插入操作：https://leetcode.cn/problems/insert-into-a-binary-search-tree/description/
+    // 89.验证二叉搜索树：https://leetcode.cn/problems/validate-binary-search-tree/description/
+    // 530.二叉搜索树的最小绝对差：https://leetcode.cn/problems/minimum-absolute-difference-in-bst/description/
+    // 530.二叉搜索树的最小距离：https://leetcode.cn/problems/minimum-distance-between-bst-nodes/description/
+    // 108.将有序数组转换为二叉搜索树: https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/description/
+    // 938.二叉搜索树的范围和: https://leetcode.cn/problems/range-sum-of-bst/description/
+    // 235.二叉搜索树的最近公共祖先: https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+    // 230.二叉搜索树中第K小的元素: https://leetcode.cn/problems/kth-smallest-element-in-a-bst/description/
+    // 173.二叉搜索树迭代器：https://leetcode.cn/problems/binary-search-tree-iterator/description/
+    // 99.恢复二叉搜索树：https://leetcode.cn/problems/recover-binary-search-tree/description/
+}
