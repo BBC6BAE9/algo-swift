2f1c9e59f1f045b62013c01ce67287e4719d59d7
huanghong <bbc6bae9@gmail.com>
2025-05-29 01:14:19 +0800

    feat(优先级队列): 完整实现

--- Algo/S1/13-PriorityQueue/PriorityQueue.swift
+++ Algo/S1/13-PriorityQueue/PriorityQueue.swift
@@ -19,7 +19,7 @@ class PriorityQueue<E: Comparable> {
     // 是否为空
     func isEmpty() -> Bool {
         return heap.isEmpty()
-    } 
+    }
     
     // 入队
     func enQueue(element: E) {
@@ -38,3 +38,24 @@ class PriorityQueue<E: Comparable> {
 }
     
 
+class Person {
+    // 名称
+    var name: String
+    // 断掉的骨头的数量，断掉的骨头越多，优先级越高
+    var boneBreak: Int
+    
+    init(name: String, boneBreak: Int) {
+        self.name = name
+        self.boneBreak = boneBreak
+    }
+}
+
+extension Person: Comparable {
+    static func < (lhs: Person, rhs: Person) -> Bool {
+        return lhs.boneBreak < rhs.boneBreak
+    }
+    
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
---
+++ AlgoTests/S1/13-PriorityQueue/PriorityQueueTests.swift
@@ -0,0 +1,26 @@
+//
+//  PriorityQueueTests.swift
+//  AlgoTests
+//
+//  Created by hong on 5/29/25.
+//
+
+import Testing
+
+struct PriorityQueueTests {
+
+    @Test func testPriorityQueue() async throws {
+        let pq = PriorityQueue<Person>()
+        pq.enQueue(element: Person(name: "Jack", boneBreak: 2))
+        pq.enQueue(element: Person(name: "Rose", boneBreak: 10))
+        pq.enQueue(element: Person(name: "Jake", boneBreak: 5))
+        pq.enQueue(element: Person(name: "James", boneBreak: 15)) 
+        
+        let person = pq.deQueue()!
+        let ret = person.name
+        let want = "James"
+        
+        assert(ret == want, "person name want \(want), but got \(ret)")
+    }
+
+}
